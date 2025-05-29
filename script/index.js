cc1b721192206875ec0bdafe4d463bcf524a32f6
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-05-29 19:48:29 +0600

    shahriar shakil

--- script/index.js
+++ script/index.js
@@ -4,10 +4,50 @@ function loadCategories(){
    .then((data)=>displayCategories(data.categories))
 }
 
+function loadVideos(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response)=>response.json())
+    .then((data)=>displayVideos(data.videos))
+}
+
+const displayVideos=(videos)=>{
+    const videoContainer=document.getElementById("video-container");
+    videos.forEach(video=>{
+        const videoCard=document.createElement("div");
+        videoCard.innerHTML=`
+         <div class="card bg-base-100 shadow-sm">
+            <figure class="relative">
+                <img class="w-full h-[250px] object-cover"
+                src="${video.thumbnail}">
+                <span class="absolute bottom-2 right-2 text-white bg-black p-1 text-sm rounded-sm">3hrs 56 min ago</span>
+            </figure>
+            <div class=" flex gap-3 px-0 py-5 ml-4">
+              
+                <div class="profile">
+                    <div class="avatar">
+                        <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring-2 ring-offset-2">
+                            <img src="${video.authors[0].profile_picture}" />
+                        </div>
+                    </div>
+                </div>
+              
+                <div class="intro">
+                   <h2 class="font-semibold">${video.title}</h2>
+                   <p class="text-sm text-gray-400 flex gap-2">${video.authors[0].profile_name}<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></p>
+                    <p class="text-sm text-gray-400">${video.others.views}</p>
+                </div>
+
+            </div>
+        </div>
+
+        `;
+        videoContainer.appendChild(videoCard)
+    })
+}
+
 function displayCategories(categories){
    const categoryContainer=document.getElementById("category-container");
    for(let cat of categories){
-    //console.log(cat);
     const categoryDiv=document.createElement("div");
     categoryDiv.innerHTML=`
     <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
@@ -17,3 +57,4 @@ function displayCategories(categories){
 }
 
 loadCategories();
+loadVideos();
